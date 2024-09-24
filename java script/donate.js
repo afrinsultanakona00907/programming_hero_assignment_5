9cb874c91635938e6d2faff1a72bac16fd552d2a
Afrin kona <152356092+afrinsultanakona00907@users.noreply.github.com>
2024-09-24 10:17:42 +0600

    first commit

---
+++ java script/donate.js
@@ -0,0 +1,22 @@
+function calculate(a, x, y, z, where){
+    document.getElementById(a).addEventListener("click", function(event){
+        event.preventDefault();
+        let amount = getAmount(x);
+        if(amount <= 0 || isNaN(amount)){
+            alert("Invalid Donation Amount!");
+        }
+        else{
+            let newAmount = getText(z) + parseFloat(amount);
+            let totalBalance = getText(y);
+            let result = totalBalance - amount;
+            document.getElementById(y).textContent = result;
+            document.getElementById(z).textContent = newAmount;
+            document.getElementById(x).value = "";
+            makeHistory(amount, getFundName(where));
+        }
+    })
+}
+
+calculate("donate-btn1", "amount1", "total-balance", "donated-money1", "topic1");
+calculate("donate-btn2", "amount2", "total-balance", "donated-money2", "topic2");
+calculate("donate-btn3", "amount3", "total-balance", "donated-money3", "topic3");
